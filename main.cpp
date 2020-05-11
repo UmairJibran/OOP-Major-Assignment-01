55a10a074225c3c6968bd532cf5ab89831eefc7a
Umair Jibran <umairjibran7@gmail.com>
2020-05-11 00:06:33 +0000

    fixed problems

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ class Subject{
     double obtainedMarks;
   public:
     Subject(){
-      this->totalMarks = 0.0;
+      this->totalMarks = 100.0;
       this->obtainedMarks = 0.0;
     }
 };
@@ -50,16 +50,10 @@ class Student: protected Physics,protected Urdu,protected ComputerScience{
       this->Physics::pracitcalObtainedMarks = physicsPMarks;
     }
     void getAveragePercentMarks(){
-      double averageObtainedMarksWithoutPracticle = 0.0;
-      double averageObtainedMarksWithPracticle = 0.0;
       double percentageObtainedMarksWithoutPracticle = 0.0;
       double percentageObtainedMarksWithPracticle = 0.0;
-      averageObtainedMarksWithoutPracticle = this->Urdu::obtainedMarks + this->Physics::obtainedMarks + this->ComputerScience::obtainedMarks / 3;
-      averageObtainedMarksWithPracticle = this->Urdu::obtainedMarks + this->Physics::obtainedMarks + this-> Physics::pracitcalObtainedMarks + this-> ComputerScience::pracitcalObtainedMarks + this->ComputerScience::obtainedMarks / 5;
-      percentageObtainedMarksWithoutPracticle = (this->Urdu::obtainedMarks + this->Physics::obtainedMarks + this->ComputerScience::obtainedMarks) / 300 * 100;
-      percentageObtainedMarksWithPracticle = (this->Urdu::obtainedMarks + this->Physics::obtainedMarks + this->ComputerScience::obtainedMarks + this-> ComputerScience::pracitcalObtainedMarks + this->Physics::pracitcalObtainedMarks) / 500 * 100;
-      cout << endl << "Average Marks of all subjects excluding Practicle: " << averageObtainedMarksWithoutPracticle;
-      cout << endl << "Average Marks of all subjects including the ones with Practicle: " << averageObtainedMarksWithPracticle;
+      percentageObtainedMarksWithoutPracticle = (this->Urdu::obtainedMarks + this->Physics::obtainedMarks + this->ComputerScience::obtainedMarks) / (this->Urdu::totalMarks + this->Physics::totalMarks + this->ComputerScience::totalMarks) * 100;
+      percentageObtainedMarksWithPracticle = (this->Urdu::obtainedMarks + this->Physics::obtainedMarks + this->ComputerScience::obtainedMarks + this-> ComputerScience::pracitcalObtainedMarks + this->Physics::pracitcalObtainedMarks) / (this->Urdu::totalMarks + this->Physics::totalMarks + this->ComputerScience::totalMarks + this-> ComputerScience::practicalTotalMarks + this->Physics::practicalTotalMarks) * 100;
       cout << endl << "Percentage of all subjects excluding Practicle: " << percentageObtainedMarksWithoutPracticle << "%";
       cout << endl << "Percentage of all subjects including the ones with Practicle: " << percentageObtainedMarksWithPracticle << "%";
     }
@@ -67,9 +61,7 @@ class Student: protected Physics,protected Urdu,protected ComputerScience{
 
 int main() {
   Student awais;
-  awais.getTheoryMakrs(90,80,85);
-  awais.getPracticalMarks(75,83);
+  awais.getTheoryMakrs(91,68,72);
+  awais.getPracticalMarks(7,83);
   awais.getAveragePercentMarks();
-  cout << endl << endl <<  "------------------------------";
-  cout << endl << "Final Statement";
 }
